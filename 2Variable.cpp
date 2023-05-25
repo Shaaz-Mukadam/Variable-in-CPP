5edc393f1214417db4a55425290dac1a76ddc437
Shaaz Mukadam <84740787+Shaaz-Mukadam@users.noreply.github.com>
2023-05-26 01:28:00 +0530

    C++ variable code and theory

---
+++ 2Variable.cpp
@@ -0,0 +1,15 @@
+//********Variable**********
+//variable is used to store the value
+//Syntax: datatypes variable_list
+//E.g: int a;
+//A variable can have alphabets, digits and underscore. A variable name can start with alphabet and underscore only. It can't start with digit.
+//No white space is allowed within variable name. variable name must not be any reserved word or keyword e.g. char, float etc.
+//Variable name can be anything
+#include <iostream>
+using namespace std;
+
+int main() {
+ int x=10;
+ cout<<x;
+ return 0;
+}
